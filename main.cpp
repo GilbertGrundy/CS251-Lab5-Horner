dcbf7dba1b90ffa5e5791f022cb7f10e935c2fbf
Gilbert E F Grundy <46936337+GilbertGrundy@users.noreply.github.com>
2019-03-14 16:52:06 -0700

    Update main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/*
+Name: Gilbert Grundy
+Assignment: Lab #5
+*/
+
+#include <iostream>
+#include <fstream>
+#include <cstring>
+
+int main()
+{
+  cout << "Gilbert Grundy CS251 Lab #4.\n";
+
+  return 0;
+}
